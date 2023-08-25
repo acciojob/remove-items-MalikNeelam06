9004abb317d221181ce99555ddee314b4eb411e4
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-25 17:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
 let option1 = document.getElementById("colorSelect");
-let form = document.getElementsByTagName("form")[0];
-let button = form["Select and Remove"];
+
 button.addEventListener("click",()=>{
-	option1.remove();
+	option1.remove(option1.selectedIndex);
 });
+// var element = document.getElementById("id_dropdown");
+         // element.remove(element.selectedIndex);
