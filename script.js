a3742a015be84ab68e604ebfdb3004504c913317
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-25 16:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let option1 = document.getElementById("colorSelect");
+let form = document.getElementsByTagName("form")[0];
+let button = form["Select and Remove"];
+button.addEventListener("click",()=>{
+	option1.remove();
+});
