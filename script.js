fa2e021c1fc0186c3e464326e0e4f19990ffea02
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-25 18:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let option1 = document.getElementById("colorSelect");
 let form = document.getElementById("form")[0];
-let button = form.value;
+let button = form["Select and Remove"];
 // let button = document.getElementById("btn");
 
 button.addEventListener("click",()=>{
